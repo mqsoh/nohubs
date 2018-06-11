fc63cde79d89fc655b8e96f8eaa264563f186249
Mason Staugler <mason@staugler.net>
2018-06-11 08:25:41 -0400

    Make it self-hosting.
    Given the SELF_HOSTING=true environment variable, the entrypoint will initialize a repository called `nohubs-site`. When you push to that repository, the GitWeb instance itself will be updated.

--- Dockerfile
+++ Dockerfile
@@ -7,22 +7,23 @@ RUN apt update && \
         highlight \
     && rm -rf /var/lib/apt/lists/*
 
+
+# SSH
 RUN adduser --home /git --disabled-password git
-
-COPY ./files/gitweb.conf /etc/
-
 COPY ./files/sshd_config /etc/ssh/
 COPY ./files/regenerate-host-keys /bin/
 RUN chmod +x /bin/regenerate-host-keys
 
+# GitWeb
 RUN a2enmod cgid
-RUN sed -i 's#DocumentRoot .\+#DocumentRoot /usr/share/gitweb#' /etc/apache2/sites-enabled/000-default.conf
+COPY ./files/apache.conf /etc/apache2/sites-enabled/000-default.conf
+COPY ./files/gitweb_config.perl /usr/share/gitweb/gitweb_config.perl
+
+# Repository management scripts.
 COPY ./files/mkrepo /bin/
 RUN chmod +x /bin/mkrepo
 COPY ./files/fork /bin/
 RUN chmod +x /bin/fork
-WORKDIR /root
-RUN ln -sf /var/lib/git/ ./code
 
 COPY ./files/motd /etc/
 COPY ./files/entrypoint.sh /bin/
--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -15,6 +15,28 @@ for var in _etc_ssh_ssh_host_ecdsa_key _etc_ssh_ssh_host_ecdsa_key_pub _etc_ssh_
     echo ${!var} | base64 -d > $fn
 done
 
+if test "$SELF_HOSTED" = "true" -a ! -d /git/nohubs-site; then
+    chown -R git:git /usr/share/gitweb
+    su git -c "
+        mkdir -p /git/nohubs-site;
+        cd /git/nohubs-site;
+        git init --bare --shared;
+        echo \"Self hosted; this site's assets.\" > description;
+        touch git-daemon-export-ok;
+        echo -e \"#!/bin/sh\ncd /usr/share/gitweb && git --git-dir=.git pull --rebase local master\" > hooks/post-update;
+        chmod +x hooks/post-update;
+
+        cd /usr/share/gitweb;
+        git init;
+        git config user.email docker-entrypoint@localhost;
+        git config user.name 'Docker Entrypoint';
+        git remote add local /git/nohubs-site;
+        git add .;
+        git commit -m \"Initialized from entrypoint.sh.\";
+        git push local master;
+    "
+fi
+
 service ssh start
 service apache2 start
 git daemon --listen=0.0.0.0 --reuseaddr --base-path=/git/ /git/ &
